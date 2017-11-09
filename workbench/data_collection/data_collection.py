5f369fe44a949b413e634347d1cf9251dd143daa
Jessica <jngitre@gmail.com>
2017-11-09 12:59:44 -0600

    working on comparing data, cleaned up code a bit, added some more databases

--- workbench/data_collection/data_collection.py
+++ workbench/data_collection/data_collection.py
@@ -1,14 +1,11 @@
 import json, urllib
 import pprint
 
+#gets data from the DoL cases via stanford's stopwagetheft
 url = "http://stopwagetheft.stanford.edu/api/v1/cases"
 response = urllib.urlopen(url)
 j = json.loads(response.read())
 loadedData = j['data']
-
-def getDataJ(hName):
-	for i in range(len(loadedData)):
-		return loadedData[i][hName]
 
 # gets data from the OSHA severeinjury.csv file
 injury_data = open("severeinjury.csv", 'r') 
@@ -16,6 +13,12 @@ load_data = injury_data.read()
 split_lines_data = load_data.split('\n')
 header = split_lines_data[0].split(',')
 csv_list = []
+
+food_data = open("cityofchicagofood.json", 'r')
+load_food = json.loads(food_data.read())
+
+bizlicense = open("bizlicense.json", 'r')
+loadbiz = json.loads(bizlicense.read())
 
 # separates the key info by commas
 for i in range(len(split_lines_data)):
@@ -28,18 +31,51 @@ for i in range(len(split_lines_data)):
 		if csv_list[i][j].find('/n') != -1:
 			''.join(csv_list[i][j:j+2])
 
+#retrieves DoL data by name
+def getDOLDataByHeader(hName):
+	data = []
+	for i in range(len(loadedData)):
+		data.append(loadedData[i][hName])
+	return data
+
 # returns the index of the desired header
-def getHeaderNum(hName):
+def getOSHAHeaderIndex(hName):
 	for i in range(0, len(header)):
 		if header[i] == hName:
 			return i
 
 # returns a list of the value filed under a given header for every case
 # 17000 is a filler number that represents around where the bad formatting starts
-def getColumn(hName):
+def getOSHADataByHeader(hName):
 	col = []
 	for i in range(17000):
-		col.append(csv_list[i][getHeaderNum(hName)])
+		col.append(csv_list[i][getOSHAHeaderIndex(hName)])
 	return col
 
-print getColumn('"Employer"')
+def getJSONDataByHeader(fName, hName):
+	data = []
+	for i in range(len(fName)):
+		data.append(fName[i][hName])
+	return data
+
+dol_emps  = getDOLDataByHeader("legal_name")
+osha_emps = getOSHADataByHeader('"Employer"')
+food_emps = getJSONDataByHeader(load_food, "dba_name")
+biz_names = getJSONDataByHeader(loadbiz, "doing_business_as_name")
+
+def getAllEmployers():
+	all_employers = []
+	all_employers.append(dol_emps)
+	all_employers.append(osha_emps)
+	all_employers.append(food_emps)
+	all_employers.append(biz_names)
+	return all_employers
+
+def getCrossRefedEmployers():
+	emps = []
+	for i in range(len(food_emps)):
+		if food_emps[i] in dol_emps:
+			emps.append(food_emps + str(i))
+	print emps
+
+getCrossRefedEmployers()
