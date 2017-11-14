5f18cb00ebd550dcd615b942875ed480ccebd727
Rohan Kumar <rohankumar@cs.uchicago.edu>
2017-11-14 13:53:47 -0600

    preprocessing

--- disambiguation/levenshtein.py
+++ disambiguation/levenshtein.py
@@ -1,9 +1,28 @@
 # Levenshtein Distance Calculator
 
-def levenshtein(s1, s2):
+def collapse_delimiters(s, delimiter=' '):
+    i, n = 0, len(s)
+    res = ''
+    while i < n:
+        if s[i].isalnum():
+            res += s[i]
+            i += 1
+        else:
+            res += delimiter
+            while i < n and not s[i].isalnum():
+                i += 1
+    return res
+
+
+def levenshtein(s1, s2, preprocess=False):
+    if preprocess:
+        s1 = s1.lower()
+        s2 = s2.lower()
+        s1 = collapse_delimiters(s1)
+        s2 = collapse_delimiters(s2)
+
     if len(s1) < len(s2):
         return levenshtein(s2, s1)
-
     # len(s1) >= len(s2)
     if len(s2) == 0:
         return len(s1)
@@ -17,5 +36,5 @@ def levenshtein(s1, s2):
             substitutions = previous_row[j] + (c1 != c2)
             current_row.append(min(insertions, deletions, substitutions))
         previous_row = current_row
-    
+
     return previous_row[-1]
