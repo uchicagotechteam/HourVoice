ad9b5a81c3c902184f67dd8c8b958b8b27b25fd9
Jessica <jngitre@gmail.com>
2017-11-28 15:58:26 -0600

    better matches lons and lats; still have annoying rounding. code a little bit cleaner

--- workbench/data_collection/data_collection.py
+++ workbench/data_collection/data_collection.py
@@ -71,24 +71,35 @@ def isFloat(str):
         	float(str)
         	return True
         except ValueError:
-                return False
+            return False
 
 dol_emps     = getJSONDataByHeader(loadedData, "legal_name")
 osha_emps    = map(toLower,  getCSVDataByHeader(osha_data, osha_header, 'Employer'))
 biz_licenses = map(toLower , getCSVDataByHeader(biz_data, biz_header, "DOING BUSINESS AS NAME"))
 food_emps    = map(toLower , getJSONDataByHeader(load_food, 'dba_name'))
-biz_lats_for_OSHA = map(roundMapOSHA, map(float, filter(isFloat, getCSVDataByHeader(biz_data, biz_header, "LATITUDE")[1:])))
-biz_lons_for_OSHA = map(roundMapOSHA, map(float, filter(isFloat, getCSVDataByHeader(biz_data, biz_header, "LONGITUDE")[1:])))
-biz_lats_for_food = map(roundMapFood, map(float, filter(isFloat, getCSVDataByHeader(biz_data, biz_header, "LATITUDE")[1:])))
-biz_lons_for_food = map(roundMapFood, map(float, filter(isFloat, getCSVDataByHeader(biz_data, biz_header, "LONGITUDE")[1:])))
+
+lats = getHeaderIndex(biz_header, "LATITUDE")
+lons = getHeaderIndex(biz_header, "LONGITUDE")
+names = getHeaderIndex(biz_header, "DOING BUSINESS AS NAME")
+for i in biz_data:
+	if isFloat(i[lats]):
+		i[lats] = float(i[lats])
+	else:
+		i[lats] = 0.0
+	if isFloat(i[lons]):
+		i[lons] = float(i[lons])
+	else:
+		i[lons] = 0.0
+
+biz_lats_for_OSHA = map(roundMapOSHA, map(float, getCSVDataByHeader(biz_data, biz_header, "LATITUDE")[1:]))
+biz_lons_for_OSHA = map(roundMapOSHA, map(float, getCSVDataByHeader(biz_data, biz_header, "LONGITUDE")[1:]))
+biz_lats_for_food = map(roundMapFood, map(float, getCSVDataByHeader(biz_data, biz_header, "LATITUDE")[1:]))
+biz_lons_for_food = map(roundMapFood, map(float, getCSVDataByHeader(biz_data, biz_header, "LONGITUDE")[1:]))
 food_lats    = map(roundMapFood, map(float, getJSONDataByHeader(load_food, 'latitude')))
 food_lons    = map(roundMapFood, map(float, getJSONDataByHeader(load_food, 'longitude')))
 osha_lats    = getCSVDataByHeader(osha_data, osha_header, 'Latitude')[1:]
 osha_lons    = getCSVDataByHeader(osha_data, osha_header, 'Longitude')[1:]
 osha_cities  = getCSVDataByHeader(osha_data, osha_header, 'City')
-violation_info = getJSONDataByHeader(load_food, 'violations')
-risk_info = getJSONDataByHeader(load_food,'risk')
-food_inspection_result = getJSONDataByHeader(load_food, 'results')
 
 def getOSHAChicago():
 	indices = []
@@ -111,20 +122,18 @@ def getCrossRefedEmployers():
 	return emps
 
 def countUpViolators():
-	cross_refed = getCrossRefedLocations()
+	(cross_refed, oshadata) = getCrossRefedLocations()
 	names = []
 	final1 = []
 	final = ""
-	(oshalats, oshalons) = getOSHAChicago()
-	oshadata = []
-	for i in oshalats:
-		if isFloat(osha_data[i[1]][9]):
-			osha_data[i[1]][9] = str(osha_data[i[1]][9])
-			oshadata.append(osha_data[i[1]])
+	for i in oshadata:
+		if isFloat(i[9]) and isFloat(i[10]):
+			i[9] = str(i[9])
+			i[10] = str(i[10])
 	for item in cross_refed:
 		names.append(item[0])
 	for i in cross_refed:
-		final1.append([biz_data[i[1]] + [str(names.count(i[0]))] + oshadata[i[2]] + [str(i[3])]])
+		final1.append([ i[2] + [str(names.count(i[0]))] + i[1] +[str(i[3])]])
 	for j in final1:
 		final = final + (','.join(j[0])) + '\n'
 	return final
@@ -137,22 +146,29 @@ def getCrossRefedLocations():
 	oshadata = []
 	for i in oshalats:
 		oshalaforcomp.append(float(i[0]))
-		if isFloat(osha_data[i[1]][9]):
-			osha_data[i[1]][9] = float(osha_data[i[1]][9])
-			oshadata.append(osha_data[i[1]])
+		if isFloat(osha_data[i[1]][9]) and isFloat(osha_data[i[1]][10]):
+			osha_data[i[1]][9]  = float(osha_data[i[1]][9])
+			osha_data[i[1]][10] = float(osha_data[i[1]][10])
+		oshadata.append(osha_data[i[1]])
 	for j in oshalons:
 		oshaloforcomp.append(float(j[0]))
-	for i in range(len(biz_lats_for_OSHA)):
-		if (biz_lats_for_OSHA[i] in oshalaforcomp) and (biz_lons_for_OSHA[i] in set(oshaloforcomp)):
-			for j in range(len(oshadata)):
-				print (oshadata[j][9],biz_lats_for_OSHA[i], oshadata[j][9] == biz_lats_for_OSHA[i])
-				if oshadata[j][9] == biz_lats_for_OSHA[i]:
-					oshaindex = j
-			locs.append((biz_licenses[i], i, oshaindex, 0))
+	for i in biz_data:
+		blats = round(float(i[lats]),2)
+		blons = round(float(i[lons]),2)
+		if (blats in set(oshalaforcomp)) and (blons in set(oshaloforcomp)):
+			for j in oshadata:
+				if j[9] == blats and j[10] == blons:
+					i[lats] = str(i[lats])
+					i[lons] = str(i[lons])
+					j[9] = str(j[9])
+					j[10] = str(j[10])
+					print (i[names], j[9], blats, i, j)
+					locs.append((i[names], i, j, 0))
 	for i in range(len(biz_lats_for_food)):
 		if (biz_lats_for_food[i] in set(food_lats)) and (biz_lons_for_food[i] in set(food_lons)):
-			locs.append((biz_licenses[i], i, 0 ,i))
-	return locs
-
+			biz_data[i][lats] = str(biz_data[i][lats])
+			biz_data[i][lons] = str(biz_data[i][lons])
+			locs.append((biz_licenses[i],biz_data[i],[],i))
+	return (locs, oshadata)
 		
 outputdata.write(countUpViolators())
