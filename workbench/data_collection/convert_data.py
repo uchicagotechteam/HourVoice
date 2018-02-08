ae08f72071b56d3ee1b54f40bc52b024f7ecf148
Rohan Kumar <rohankumar@cs.uchicago.edu>
2018-02-07 23:10:07 -0600

    bug in keys of glimpse

--- workbench/data_collection/convert_data.py
+++ workbench/data_collection/convert_data.py
@@ -4,24 +4,24 @@ import json
 # num_with_freq, biz, osha, only_food_inspection = 0,0,0,0
 
 def get_subset(hourvoice_id, establishment):
-    global num_with_freq, biz, osha, only_food_inspection
-    subset = {'hourvoice_id': hourvoice_id, 'business_name': '', 'address': '', 'frequency': '', 'owner': '', 'business_license_number': '', 'zip_code': '', 'city': ''}
+    # global num_with_freq, biz, osha, only_food_inspection
+    subset = {'hourvoice_id': hourvoice_id, 'name': '', 'address': '', 'frequency': '', 'owner': '', 'business_license_number': '', 'zip_code': '', 'city': ''}
     if 'Frequency' in establishment:
         subset['frequency'] = establishment['Frequency']
-        num_with_freq += 1
+        # num_with_freq += 1
     if 'business_license' in establishment:
-        biz += 1
+        # biz += 1
         subset['address'] = establishment['business_license']['address']
-        subset['business_name'] = establishment['business_license']['doing_business_as_name']
+        subset['name'] = establishment['business_license']['doing_business_as_name']
         subset['zip_code'] = establishment['business_license']['zip_code']
         subset['city'] = establishment['business_license']['city']
     elif 'OSHA' in establishment:
-        osha += 1
-        subset['business_name'] = establishment['OSHA']['Employer']
+        # osha += 1
+        subset['name'] = establishment['OSHA']['Employer']
         subset['zip_code'] = establishment['OSHA']['Zip']
         subset['city'] = establishment['OSHA']['City']
     elif 'food_inspection' in establishment:
-        only_food_inspection += 1
+        # only_food_inspection += 1
         subset['name'] = establishment['food_inspection']['dba_name']
         subset['zip_code'] = establishment['food_inspection'].get('zip','')
         subset['address'] = establishment['food_inspection']['address']
